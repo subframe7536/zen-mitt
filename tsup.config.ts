05edd6460fae187c872073e1e603fa5b503d9fed
subframe7536 <1667077010@qq.com>
2025-02-19 09:37:30 +0800

    feat: add min build and export dist

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  dts: true,
-  clean: true,
-  format: ['esm', 'cjs'],
-  entry: {
-    index: './src/index.ts',
-    class: './src/class.ts',
+export default defineConfig([
+  {
+    dts: true,
+    clean: true,
+    format: ['esm', 'cjs'],
+    entry: {
+      index: './src/index.ts',
+      class: './src/class.ts',
+    },
+    external: ['vite', 'esbuild'],
+    outDir: 'dist',
   },
-  external: ['vite', 'esbuild'],
-  outDir: 'dist',
-})
+  {
+    format: ['esm'],
+    entry: {
+      'index.min': './src/index.ts',
+      'class.min': './src/class.ts',
+    },
+    minify: true,
+    splitting: false,
+    external: ['vite', 'esbuild'],
+    outDir: 'dist',
+  },
+])
